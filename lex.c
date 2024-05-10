97e3450f333430fdf46687a15746507c4e12426a
Breno Martins <brenoms2001@gmail.com>
2024-05-09 23:49:05 -0300

    Fix: palavras reservadas identificadas com sucesso

--- lex.c
+++ lex.c
@@ -23,48 +23,62 @@ int main(void){
     return 0;
 }
 
-int proxtoken(void){
+int proxtoken(void) {
     //simbolos
-    if(c=='+'||c=='-'||c=='*'||c=='/'||c=='='||c==';'||c=='%'||c=='('||c==')'||c=='{'||c=='}'){
+    if (c == '+' || c == '-' || c == '*' || c == '/' || c == '=' || c == ';' || c == '%' || c == '(' || c == ')' || c == '{' || c == '}') {
         printf("%c\n", c);
         c = getchar();
         return c;
     }
 
     //letras
-    else if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
-        for(int i = 0; (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'); i++){
+    else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+        int i;
+        for(i = 0; (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'); i++){
             token[i] = c;
             c = getchar();
         }
-        if(strcmp(token, "if") || strcmp(token, "while") || strcmp(token, "else"))
+        token[i] = '\0';
+
+        //palavras reservadas
+        if (strcmp(token, "if") == 0 || strcmp(token, "while") == 0 || strcmp(token, "else") == 0) {
+            printf("%s\n", token);
+            clearToken(token);
+            return c;
+        }
+        
+        //outras palavras
         printf("NOME %s\n", token);
-        c = getchar();
         clearToken(token);
         return c;
     }
 
-    //numeros
-    else if(c >= '0' && c <= '9'){
-        for(int i = 0; c >= '0' && c <= '9'; i++){
+    // numeros
+    else if (c >= '0' && c <= '9') {
+        int i;
+        for (i = 0; c >= '0' && c <= '9'; i++){
             token[i] = c;
             c = getchar();
         }
+        token[i] = '\0';
         printf("NUM %s\n", token);
-        c = getchar();
         clearToken(token);
         return c;
     }
 
     //espacos
-    else{
+    else {
         c = getchar();
         return c;
     }
 }
+
     
 void clearToken(char* completeToken){
     for(int i = 0; i < MAX; i++){
+        if(token[i] == ' '){
+            break;
+        }
         token[i] = ' ';
     }
 }
